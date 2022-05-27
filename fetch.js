0bb62e4f50223a93f46f2573cd01031be8b4a8d7
Juan Guillermo Quintana <juan.quintana@conekta.com>
2022-05-27 18:38:30 -0500

    clase de fetch

--- fetch.js
+++ fetch.js
@@ -1,30 +1,19 @@
 const pokemon = document.querySelector('#pokemon')
 
-function renderPokemon(image) {
-    pokemon.setAttribute('src', image)
+let renderPokemon= (image) =>{
+    pokemon.setAttribute('src',image)
 }
 
-fetch('pokemon.json', {
-
-})
-    .then((response) => {
-        return response.json()
-    })
-    .then((data) => {
-        //debugger
-        renderPokemon(data[0].image)
-    })
-/*
-fetch('url',{
+fetch('https://pokeapi.co/api/v2/pokemon/pikachu',{
     method:'GET',
     headers:{
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json'
     }
-    body: JSON.stringify({
-        name:'juan',
-        age:29,
-    })
 })
-.then
-.catch
-*/
+.then((response) =>{
+    return response.json()
+})
+.then((data)=>{
+    debugger
+    renderPokemon(data.sprites.back_default)
+})
